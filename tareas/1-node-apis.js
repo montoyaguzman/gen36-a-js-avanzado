90477e519a24abfbd7cc826e0bd49486309661d7
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-09-09 21:32:56 -0600

    feat: primer api / server con node

---
+++ tareas/1-node-apis.js
@@ -0,0 +1,6 @@
+1. Definir con sus propias palabras que es node y para que se usa.
+2. Repasar los conceptos de api. 
+3. Probar su servidor con otro status y otro mymeType 
+    Que paso ?
+    Cuales fueron los resultados ?
+        A que crees qu se debe ?
