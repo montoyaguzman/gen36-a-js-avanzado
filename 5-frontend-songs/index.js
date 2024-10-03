074c76451044664249a129b5bbf8f7dde0c59ce4
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-10-02 19:53:15 -0600

    feat: ejercicio de post en el front

--- 5-frontend-songs/index.js
+++ 5-frontend-songs/index.js
@@ -1,8 +1,59 @@
 const URL = "http://localhost:3000/songs";
 const ulElement = document.querySelector('ul');
+const registerSongForm = document.querySelector('#registerSongForm');
+let songs = [];
+
+registerSongForm.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    // formData
+
+    const songInputElement = document.querySelector('#songName');
+    const artistInputElement = document.querySelector('#artistName');
+
+    const song = songInputElement.value;
+    const artist = artistInputElement.value;
+
+    const body = {
+        id: songs.length + 1,
+        name: song,
+        artist: artist,
+    };
+
+    createSongs(body);
+
+});
+
+const createSongs = async (body) => {
+    try {
+
+        // options: configuracion que necesita fetch cuando no un GET
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        };
+        // const newUrl = `${URL}/10?genre=banda&anio=2020`;
+        const response = await fetch(URL, options);
+        // jsonResponse = await response.json(); // vacio 201
+
+        if (response.status === 201) {
+            alert('Cancion creada');
+            const songs = await getSongs();
+            renderSongs(songs);
+        } else {
+            alert('hay un error');
+        }
+
+    } catch (error) {
+        console.error(error.message);
+    }
+
+};
 
 const getSongs = async () => {
-    let songs = [];
+    ulElement.innerHTML = '';
+    songs = [];
     try {
         const response = await fetch(URL);
         if (!response.ok) {
@@ -26,9 +77,17 @@ const renderSongs = (songs) => {
 
 };
 
+const renderEmptySongs = () => {
+    const labelElement = document.createElement('label');
+    labelElement.innerHTML = `no hay canciones disponibles por el momento...`;
+    ulElement.appendChild(labelElement);
+}
+
 const main = async () => {
-    const songs = await getSongs();
-    renderSongs(songs);
+    songs = await getSongs();
+    songs.length > 0 ? renderSongs(songs) : renderEmptySongs()
+    // manejo de excepciones, valores undefined, errores, propiedades faltantes en un json
+    // encadenamiento opcion (?.), if/else, ternario, try catch
 }
 
 main();
